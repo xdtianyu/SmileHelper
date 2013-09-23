d3771a711b1520521e733afc6f665c4b7dc7a5a2
xdtianyu <xdtianyu@gmail.com>
2013-09-23 09:40:54 +0800

    ty creat SmileHelper

---
+++ src/org/xdty/smilehelper/MainActivity.java
@@ -0,0 +1,43 @@
+package org.xdty.smilehelper;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    
+    private Context mContext;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mContext = this;
+		setContentView(R.layout.main);
+		Button startFloatWindow = (Button) findViewById(R.id.start_float_window);
+		startFloatWindow.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent(MainActivity.this, FloatWindowService.class);
+				startService(intent);
+				FloatWindowSmallView.close = false;
+				finish();
+			}
+		});
+		
+		Button stopFloatWindow = (Button) findViewById(R.id.stop_float_window);
+		stopFloatWindow.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                MyWindowManager.removeSmallWindow(getContext());
+                FloatWindowSmallView.close = true;
+            }
+        });
+	}
+	
+	private Context getContext(){
+	    return mContext;
+	}
+}
